c5282598c54e317d147ec59fa42d24a6a8ddb1c1
聂康 <964972709@qq.com>
2017-07-13 09:55:46 +0800

    创建项目

---
+++ GestureLock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GestureLock
+//
+//  Created by 聂康  on 2017/7/13.
+//  Copyright © 2017年 com.nk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GestureLockTests/GestureLockTests.m
@@ -0,0 +1,39 @@
+//
+//  GestureLockTests.m
+//  GestureLockTests
+//
+//  Created by 聂康  on 2017/7/13.
+//  Copyright © 2017年 com.nk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GestureLockTests : XCTestCase
+
+@end
+
+@implementation GestureLockTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
